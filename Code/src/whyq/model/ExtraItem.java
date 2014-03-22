84b938c9e4bde1371462ec8897b7fa97bc217016
thiendv <thienuit@gmail.com>
2014-03-22 14:16:45 +0700

    New miit

---
+++ Code/src/whyq/model/ExtraItem.java
@@ -0,0 +1,180 @@
+package whyq.model;
+
+import java.io.Serializable;
+
+public class ExtraItem implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String productId;
+	private String name;
+	private String skue;
+	private String value;
+	private String status;
+	private String type;
+	private String note;
+	private String sort;
+	private String createdata;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @return the createdata
+	 */
+	public String getCreatedata() {
+		return createdata;
+	}
+
+	/**
+	 * @param createdata
+	 *            the createdata to set
+	 */
+	public void setCreatedata(String createdata) {
+		this.createdata = createdata;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status
+	 *            the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the note
+	 */
+	public String getNote() {
+		return note;
+	}
+
+	/**
+	 * @param note
+	 *            the note to set
+	 */
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	/**
+	 * @return the sort
+	 */
+	public String getSort() {
+		return sort;
+	}
+
+	/**
+	 * @param sort
+	 *            the sort to set
+	 */
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value
+	 *            the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the skue
+	 */
+	public String getSkue() {
+		return skue;
+	}
+
+	/**
+	 * @param skue
+	 *            the skue to set
+	 */
+	public void setSkue(String skue) {
+		this.skue = skue;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the productId
+	 */
+	public String getProductId() {
+		return productId;
+	}
+
+	/**
+	 * @param productId
+	 *            the productId to set
+	 */
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
