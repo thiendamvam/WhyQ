faf22715a9b4560f31563413ced188223d3f331f
md101 <md101@10.0.0.33>
2014-06-01 16:08:16 +0700

    resize comment layout incase no avatar

--- Code/src/whyq/activity/CommentActivity.java
+++ Code/src/whyq/activity/CommentActivity.java
@@ -252,10 +252,12 @@ public class CommentActivity extends ImageWorkerActivity {
 //						holder.avatar);
 				mImageLoader.DisplayImage(item.getUser().getUrlAvatar(), holder.avatar);
 				Photo photo = item.getPhotos();
-				if (photo != null) {
+				if (photo != null && photo.getThumb() != null) {
 //					mImageWorker.downloadImage(item.getPhotos().getThumb(),
 //							holder.thumb);
 					mImageLoader.DisplayImage(item.getPhotos().getThumb(), holder.thumb);
+				}else{
+					holder.thumb.setVisibility(View.GONE);
 				}
 				if (item.getUser().getLike() > 0) {
 					holder.favorite.setImageResource(R.drawable.icon_fav_enable);
