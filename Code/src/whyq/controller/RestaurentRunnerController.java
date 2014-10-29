2e8d3f2d440d145335c4cd16d2d636749b2baa3c
EVA ThienDV <thiendv@evolableasia.vn>
2014-10-29 14:19:30 +0700

    Fix restaurent rr

---
+++ Code/src/whyq/controller/RestaurentRunnerController.java
@@ -0,0 +1,18 @@
+package whyq.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import whyq.model.Bill;
+
+public class RestaurentRunnerController{
+	
+	public static Map<String, Map<String, List<Bill>>> restaurentList = new HashMap<String, Map<String,List<Bill>>>();
+	
+	public RestaurentRunnerController() {
+		// TODO Auto-generated constructor stub
+	}
+		
+
+}
